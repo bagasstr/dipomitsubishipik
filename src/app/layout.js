089a8931e6fa0f627d598f1f6ddf725caeafcf46
bagasstr <bagassatrio2805@gmail.com>
2024-09-23 00:40:30 +0700

    First commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import Navbar from "@/component/organisms/Navbar";
 import "./globals.css";
 import { Poppins } from "next/font/google";
 import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
+import robots from "./robots";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -11,6 +12,9 @@ const poppins = Poppins({
 
 export const metadata = {
   title: "Dipo Mistubishi Pik",
+  verification: {
+    google: "googled81ade8dd415c7d1.html",
+  },
   description:
     "Dealer Resmi Mistubishi. Temukan mobil Mitsubishi impian Anda di sini! Kami menawarkan berbagai pilihan mobil baru dengan harga terbaik dan promo menarik. Dapatkan layanan sales yang profesional dan terpercaya.",
   keywords:
@@ -19,11 +23,11 @@ export const metadata = {
     title: "Dipo Mistubishi Pik",
     description:
       "Dealer Resmi Mitsubishi. Temukan mobil Mitsubishi impian Anda di sini!",
-    url: "https://dipomitsubishi.com/",
+    url: "https://dipomitsubishi.vercel.app/",
     siteName: "Dipo Mistubishi Pik",
     images: [
       {
-        url: "https://dipomitsubishi.com/",
+        url: "https://dipomitsubishi.vercel.app/",
         width: 1920,
         height: 1080,
       },
@@ -31,15 +35,30 @@ export const metadata = {
     locale: "en-US",
     type: "website",
   },
-
-  verification: {
-    google: "googled81ade8dd415c7d1.html",
-  },
   authors: [
     { name: "Bagas Satrio", url: "https://linkedin.com/id/bagasstr/" },
-    { name: "Dipo Mistubishi Pik", url: "https://dipomitsubishi.com/" },
+    { name: "Dipo Mistubishi Pik", url: "https://dipomitsubishi.vercel.app/" },
   ],
-  category: "Mitsubishi, Pajero Sport, Triton, Xpander Cross, Xforce ",
+  category: "cars",
+  viewport: {
+    width: "device-width",
+    initialScale: 1.0,
+    maximumScale: 1.0,
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  creator: "Bagas Satrio",
+  publisher: "Bagas Satrio",
 };
 
 export default function RootLayout({ children }) {
